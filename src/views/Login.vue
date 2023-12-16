66ba2dc01d1141c2f0f2f1cabe2f1e859ba8f36e
Kgotso Phiri <kgotsokg12@gmail.com>
2023-12-16 13:26:31 +0200

    Add login layout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,50 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div>
-        <p>Login View</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "login",
-    setup() {
+  <div class="max-w-screen-sm mx-auto px-4 py-10">
+      <!-- Error Handling -->
+      <div v-if="errorMsg" class="mb-10 p-4 rounded-md bg-light-grey">
+          <p class="text-red-500">{{ errorMsg }}</p>
+      </div>
+  </div>
+
+  <!-- Register Form -->
+  <form class="max-w-screen-sm mx-auto p-8 flex flex-col bg-light-grey rounded-md shadow-lg">
+      <h1 class="text-3xl text-at-light-green mb-4">Login</h1>
+
+      <div class="flex flex-col mb-2">
+          <label for="email" class="mb-1 text-sm text-at-light-green">Email</label>
+          <input type="email" placeholder="name@email.com" name="email" required
+              class="p-2 text-gray-500 focus:outline-none" v-model="email" id="password">
+      </div>
+
+      <div class="flex flex-col mb-2">
+          <label for="password" class="mb-1 text-sm text-at-light-green">Password</label>
+          <input type="password" name="password" required class="p-2 text-gray-500 focus:outline-none" v-model="password"
+              id="password">
+      </div>
+
+      <button type="submit" class="mt-6 py-2 px-6 rounded-sm slef-start text-sm text-white bg-at-light-green duration-200 border-solid
+      border-2 border-transparent hover:border-at-light-green hover:bg-white
+      hover:text-at-light-green">Login</button>
+
+      <router-link class="text-sm mt-6 text-center" :to="{ name: 'register' }">
+          Don't have an account? <span class="text-at-light-green">Register</span></router-link>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: "login",
+  setup() {
       // Create data / vars
-  
-      // Login function
-  
-      return {};
-    },
-  };
-  </script>
+      const email = ref(null);
+      const password = ref(null);
+      const errorMsg = ref(null);
+
+      // Register function
+      return { email, password, errorMsg };
+  },
+};
+</script>
