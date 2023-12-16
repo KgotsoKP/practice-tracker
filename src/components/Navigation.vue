f9dc1468448a9e54c132b794389841fa4431ef59
Kgotso Phiri <kgotsokg12@gmail.com>
2023-12-16 22:23:12 +0200

    Conditionally render nave links

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,9 +7,9 @@
 
       <ul class="flex flex-1 justify-end gap-x-10">
         <router-link class="cursor-pointer text-white" :to="{ name: 'home' }">Home</router-link>
-        <router-link class="cursor-pointer text-white" :to="{ name: 'register' }">Create</router-link>
-        <router-link class="cursor-pointer text-white" :to="{ name: 'login' }">Login</router-link>
-        <li @click="logout" class="cursor-pointer">Logout</li>
+        <router-link v-if="user" class="cursor-pointer text-white" :to="{ name: 'register' }">Create</router-link>
+        <router-link v-if="!user" class="cursor-pointer text-white" :to="{ name: 'login' }">Login</router-link>
+        <li v-if="user" @click="logout" class="cursor-pointer">Logout</li>
       </ul>
     </nav>
   </header>
@@ -18,20 +18,22 @@
 <script>
 import { supabase } from '@/superbase/init';
 import { useRouter } from 'vue-router';
+import { computed } from 'vue';
+import store from '@/store/index';
 
 export default {
   setup() {
     // Get user from store
-    const router = useRouter();
-
+    const user = computed(() => store.state.user)
     // Setup ref to router
+    const router = useRouter();
+    
+    // Logout function
     const logout = async () => {
       await supabase.auth.signOut();
       router.push({name: 'home'});
     }
-
-    // Logout function
-    return { logout };
+    return { logout, user };
   },
 };
 </script>
